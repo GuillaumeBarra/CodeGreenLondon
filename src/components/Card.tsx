03934a451567a863abc1f80827989836581f8f87
Guillaume Barra <guillaume.barra2@gmail.com>
2023-11-30 17:09:03 +0000

    fix: card responsiveness

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,18 +5,17 @@ import React from 'react';
 const Card = ({title, description, link, date, location}: CGLEvent) => {
   console.log("description", description)
   return (
-    <a className="w-1/4 h-[23rem] rounded-lg border-2 overflow-hidden m-4 breakpoint:w-full"
+    <a className="w-1/4 h-[23rem] rounded-lg border-2 overflow-hidden m-4 breakpoint:w-full breakpoint:h-[10rem]"
     href={link}
     target="_blank">
 
       <div className="relative p-4 bg-transparent h-full">
         <h3 className="text-xl font-bold mb-2">{title}</h3>
-        <p className=" breakpoint:hidden">{description}</p>
+        <p className="breakpoint:hidden">{description}</p>
         <div className='absolute inset-x-0 bottom-0 p-4'>
           <p>{formatDate(date)}</p>
-          <p className=" breakpoint:hidden">{location}</p>
+          <p>{location}</p>
         </div>
-        <p className=" invisible breakpoint:visible">Read more...</p>
       </div>
     </a>
   );
